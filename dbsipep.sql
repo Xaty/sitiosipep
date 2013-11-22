110756430f0cc677cf513c3c2158b1ea074f06f3
upastor <uripast_@hotmail.com>
2013-11-22 16:55:58 -0600

    Modificando BD para borrar en cascada

--- dbsipep.sql
+++ dbsipep.sql
@@ -3,9 +3,15 @@ create database if not exists sipep;
 
 use sipep;
 
+create table usuario(
+	id_usuario int primary key,
+	usuario varchar(15) not null,
+	clave varchar(15) not null,
+	rol varchar(5) not null
+);
 
 create table egresado(
-	id_egresado varchar(10) not null,
+	id_egresado int,
 	nombre varchar(30) not null,
 	apellido_paterno varchar(30) not null,
 	apellido_materno varchar(30) not null,
@@ -16,7 +22,10 @@ create table egresado(
 	direccion varchar(50),
 	fNacimiento date,
 	cedulaProfesional varchar(10),
-	fotografia longblob
+	fotografia longblob,
+	primary key (id_egresado),
+	id_usuario int,
+	foreign key (id_usuario) references usuario(id_usuario) on delete cascade
 );
 
 
@@ -40,13 +49,6 @@ create table oferta(
 	empresa varchar(30)
 );
 
-create table usuario(
-	id_usuario int primary key,
-	usuario varchar(15) not null,
-	clave varchar(15) not null,
-	rol varchar(5) not null
-);
-
 create table rol(
 	rol int
 );
@@ -54,7 +56,9 @@ create table rol(
 create table curriculum(
 	id_curriculum int,
 	habilidades varchar(50) not null,
-	primary key (id_curriculum)
+	primary key (id_curriculum),
+	id_egresado int,
+	foreign key (id_egresado) references egresado(id_egresado) on delete cascade
 );
 
 create table experiencia(
@@ -66,7 +70,7 @@ create table experiencia(
 	funciones varchar(50),
 	logros varchar(60),
 	id_curriculum int,
-	foreign key (id_curriculum) references curriculum(id_curriculum)
+	foreign key (id_curriculum) references curriculum(id_curriculum) on delete cascade
 );
 
 create table curso(
@@ -75,7 +79,7 @@ create table curso(
 	institucion varchar(30) not null,
 	periodo_curso varchar(15) not null,
 	id_curriculum int,
-	foreign key (id_curriculum) references curriculum(id_curriculum)
+	foreign key (id_curriculum) references curriculum(id_curriculum) on delete cascade
 );
 
 
@@ -85,7 +89,7 @@ create table formacion_academica(
 	carrera varchar(20) not null,
 	especialidad varchar(20),
 	id_curriculum int,
-	foreign key (id_curriculum) references curriculum(id_curriculum)
+	foreign key (id_curriculum) references curriculum(id_curriculum) on delete cascade
 );
 
 create table idioma(
@@ -93,6 +97,32 @@ create table idioma(
 	idioma varchar(10) not null,
 	nivel varchar(10) not null,
 	id_curriculum int,
-	foreign key (id_curriculum) references curriculum(id_curriculum)
+	foreign key (id_curriculum) references curriculum(id_curriculum) on delete cascade
 );
 
+insert into usuario
+values ('001','ur','erer','des');
+
+insert into egresado
+values ('1234','uriel','pastor','mendoza','ppoier123','uri@hotmail.com','M','A','hidalgo','1991-09-22','erge23425',NULL,'001');
+
+insert into empresa
+values ('123','Google','DesaSoft','macro','google@gmail.com','USA',NULL);
+
+insert into oferta
+values ('345','dfsde','ddgt','1991-09-05','sdfert','dfre','sfe');
+
+insert into curriculum
+values ('01','perioeierertor','1234');
+
+insert into curso
+values ('0001','desa','utvm','abril-mayo','01');
+
+insert into idioma
+values ('012','frances','basic','01');
+
+select * from curriculum where id_egresado="1234";
+
+delete from usuario where id_usuario = "001";
+
+select * from egresado;
